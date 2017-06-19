39d76a1dc07a0233b4c29df8cad62afd3399422d
Maarten Blomme <maartenblomme@gmail.com>
2017-06-19 22:04:54 +0200

    update client.py

--- client.py
+++ client.py
@@ -1,45 +1,101 @@
 #!/usr/bin/python
 import evdev
-import sys
+import sys, getopt
 import urllib2
 import time
 
 
-if len(sys.argv)<3:
-    print "Please specify an input device and a server url and gpio number"
-    sys.exit(-1)
-
-devicename = sys.argv[1]
-serverurl = sys.argv[2]
-gpio = sys.argv[3]
+def set_gpio(gpio, value):
+    if gpio:
+        with open('/sys/class/gpio/gpio' + gpio + '/value', 'w') as value_file:
+            value_file.write(str(value))
 
 
-def open_lock():
-    with open('/sys/class/gpio/gpio'+gpio+'/value', 'w') as value_file:
-        print "open"
-        value_file.write('1')
-        value_file.flush()
-        time.sleep(2)
-        print "close"
-        value_file.write('0')
-        value_file.flush()
+def open_lock(gpio):
+    print "open"
+    set_gpio(gpio, 1)
+    time.sleep(2)
+    print "close"
+    set_gpio(gpio, 0)
 
-def get_permission(intcode):
+
+def verify(intcode, url, gpio):
+    url = url + "/" + str(intcode)
+    print urllib2.urlopen(url).read()
+    open_lock(gpio)
+
+
+def get_permission(intcode, serverurl, backupurl, gpio):
     try:
-        url = serverurl+"/"+str(intcode)
-        print urllib2.urlopen(url).read()
-        open_lock()
+        verify(intcode, serverurl, gpio)
     except urllib2.HTTPError, e:
         print 'HTTPError = ' + str(e.code)
     except urllib2.URLError, e:
         print 'URLError = ' + str(e.reason)
+
+        # only when given server does not exist
+        if backupurl:
+            try:
+                print "trying backup server url"
+                verify(intcode, backupurl, gpio)
+            except Exception, e:
+                raise e
+
     except Exception, e:
         raise e
 
 
-def read_input():
-    keys = "X^1234567890XXXXqwertzuiopXX\nXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX"
+def help(cmd):
+    print cmd + '-i <input device> -u <server url> -b <backup server url> -g <gpio number>'
 
+
+def main(argv):
+    devicename = None
+    serverurl = None
+    backupurl = None
+    gpio = None
+
+    try:
+        opts, args = getopt.getopt(argv, "hi:u:b:g:", ["input=", "url=", "backupurl=", "gpio="])
+    except getopt.GetoptError:
+        help(sys.argv[0])
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            help(sys.argv[0])
+            sys.exit()
+        elif opt in ("-i", "--input"):
+            devicename = arg
+        elif opt in ("-u", "--url"):
+            serverurl = arg
+        elif opt in ("-b", "--backup"):
+            backupurl = arg
+        elif opt in ("-g", "--gpio"):
+            gpio = arg
+
+    if not devicename or not serverurl:
+        help(sys.argv[0])
+        sys.exit(2)
+
+    dev = evdev.InputDevice(devicename)
+    dev.grab()
+    print dev
+    print "server url: %s" % serverurl
+    print "server url backup: %s" % backupurl
+
+    if gpio:
+        print "gpio: " + gpio
+        try:
+            with open('/sys/class/gpio/export', 'w') as export_file:
+                export_file.write(gpio)
+        except:
+            pass
+
+        with open('/sys/class/gpio/gpio' + gpio + '/direction', 'w') as direction_file:
+            direction_file.write('out')
+
+    keys = "X^1234567890XXXXqwertzuiopXX\nXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX"
     code = ""
     for event in dev.read_loop():
         if event.type == 1 and event.value == 1:
@@ -52,22 +108,9 @@ def read_input():
                     print "invalid code"
                 else:
                     print intcode
-                    get_permission(intcode)
+                    get_permission(intcode, serverurl, backupurl, gpio)
                 code = ""
-                print ""
 
 
-dev = evdev.InputDevice(devicename)
-print dev
-
-try:
-    with open('/sys/class/gpio/export', 'w') as export_file:
-        export_file.write(gpio)
-except:
-    pass
-
-with open('/sys/class/gpio/gpio'+gpio+'/direction', 'w') as direction_file:
-    direction_file.write('out')
-
-read_input()
-
+if __name__ == "__main__":
+   main(sys.argv[1:])
