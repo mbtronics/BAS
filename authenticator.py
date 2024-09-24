7ebe858de0bc02ac74b11e8e992565d28b328043
Maarten Blomme <maartenblomme@gmail.com>
2024-09-24 20:57:33 +0200

    Even if access has been granted because of the cards.txt file, still notify the server

--- authenticator.py
+++ authenticator.py
@@ -15,24 +15,32 @@ class Authenticator:
             raise Exception('invalid secret key: %s' % self._secret_key)
 
     def auth(self, lock, user_id, logger):
+        access_granted = False
 
         try:
             with open('/etc/cards.txt', 'r') as cards_file:
                 for card in cards_file:
                     if card.strip == user_id:
-                        return True
+                        access_granted = True
         except:
             pass
 
-        url = "%s/auth/lock/%s/%s/%s" % (self._url, self._secret_key, lock.id, user_id)
-
         try:
             # raises exception on http authentication error
-            verify_key = urllib.request.urlopen(url, timeout=5).read().decode()
+            url = "%s/auth/lock/%s/%s/%s" % (self._url, self._secret_key, lock.id, user_id)
+
+            # even if we already have access, still notify the server, but use a lower timeout
+            timeout_s = 5
+            if access_granted:
+                timeout_s = 1
+
+            verify_key = urllib.request.urlopen(url, timeout=timeout_s).read().decode()
         except urllib.error.HTTPError as e:
             logger.error('HTTPError = ' + str(e.code))
         except urllib.error.URLError as e:
             logger.error('URLError = ' + str(e.reason))
         else:
             if verify_key == self._secret_key:
-                return True
+                access_granted = True
+
+        return access_granted
