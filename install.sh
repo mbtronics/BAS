67b5d9774106666de781def12951e8e303c11531
Maarten Blomme <maartenblomme@gmail.com>
2024-09-25 20:57:17 +0200

    update install script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ read -p "Lock ID (eg. 4): " LOCK_ID
 cp bas.service /lib/systemd/system/bas.service
 sed -i s/LOCK_GPIO/"$LOCK_GPIO"/g /lib/systemd/system/bas.service
 sed -i s/LOCK_ID/"$LOCK_ID"/g /lib/systemd/system/bas.service
-
+touch /var/log/bas.log
 systemctl daemon-reload
 systemctl enable bas
 systemctl start bas
