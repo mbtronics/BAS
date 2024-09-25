6fc6a849e3f72310d3baf1a218e28173ea92f6f6
Maarten Blomme <maartenblomme@gmail.com>
2024-09-25 20:47:14 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ read -p "Lock GPIO (eg. 203): " LOCK_GPIO
 read -p "Lock ID (eg. 4): " LOCK_ID
 read -p "RGB (eg. 3,2,0): " RGB
 cp bas.service /lib/systemd/system/bas.service
-sed -i 's/LOCK_GPIO/$LOCK_GPIO/g' /lib/systemd/system/bas.service
-sed -i 's/LOCK_ID/$LOCK_ID/g' /lib/systemd/system/bas.service
-sed -i 's/RGB/-r RGB/g' /lib/systemd/system/bas.service
+sed -i "s/LOCK_GPIO/$LOCK_GPIO/g" /lib/systemd/system/bas.service
+sed -i "s/LOCK_ID/$LOCK_ID/g" /lib/systemd/system/bas.service
+sed -i "s/RGB/-r RGB/g" /lib/systemd/system/bas.service
 
 systemctl daemon-reload
 systemctl enable bas
