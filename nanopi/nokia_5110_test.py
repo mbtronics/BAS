53b9db13a89843c9bd9d455120cd74d215be9d90
Maarten Blomme <maartenblomme@gmail.com>
2025-02-22 15:57:01 +0100

    nokia_5110 script: proper loop handling

--- nanopi/nokia_5110_test.py
+++ nanopi/nokia_5110_test.py
@@ -2,15 +2,19 @@ from luma.core.interface.serial import spi
 from luma.core.render import canvas
 from luma.lcd.device import pcd8544
 import time
+import sys
 
 # Initialize the display
 serial = spi(port=0, device=0, gpio_DC=201, gpio_RST=1)
 device = pcd8544(serial, rotate=0, gpio_LIGHT=202)
 
-# Draw something
-with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="white", fill="black")
-    draw.text((10, 10), "Hello World", fill="red")
-
-print("sleep")
-time.sleep(5)
+try:
+    # Draw something
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((10, 10), "Hello World", fill="red")
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Ctrl-C pressed!")
+    sys.exit(0)
