1722a598fa85db6c6c05ec1f699444d0e23319a9
Maarten Blomme <maartenblomme@gmail.com>
2025-02-22 16:22:47 +0100

    Fix backlight

--- nanopi/nokia_5110_test.py
+++ nanopi/nokia_5110_test.py
@@ -6,13 +6,16 @@ import sys
 
 # Initialize the display
 serial = spi(port=0, device=0, gpio_DC=201, gpio_RST=1)
-device = pcd8544(serial, rotate=0, gpio_LIGHT=202)
+device = pcd8544(serial, rotate=0, gpio_LIGHT=200, active_low=False)
+device.backlight(False)
 
 try:
+    device.backlight(True)
+
     # Draw something
     with canvas(device) as draw:
         draw.rectangle(device.bounding_box, outline="white", fill="black")
-        draw.text((10, 10), "Hello World", fill="red")
+        draw.text((10, 10), "Hello World", fill="white", stroke_fill="black")
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
